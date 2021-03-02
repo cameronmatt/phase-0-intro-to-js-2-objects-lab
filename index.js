5b6b9cff99f4ddd71d328a6e417994a4cc8baa03
Matthew <cameron.matt@outlook.com>
2021-03-03 00:18:10 +1100

    Objects lab done

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your solution in this file!
+let employee = {
+    name: 'Sam',
+    streetAddress: '11 Broadway',
+    }
+function updateEmployeeWithKeyAndValue(employee, streetAddress, value){
+return Object.assign({},employee,{[streetAddress]: '11 Broadway'})
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, streetAddress, value){
+employee[streetAddress] = '12 Broadway'
+return employee
+}
+function deleteFromEmployeeByKey(employee, name){
+let newemployee =  Object.assign({},employee)
+delete newemployee[name]
+return newemployee}
+function destructivelyDeleteFromEmployeeByKey(employee, name){
+delete employee[name]
+return employee
+}
+
